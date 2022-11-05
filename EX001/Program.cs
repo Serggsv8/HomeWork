8853011d37d229c188234181f175995e848b4f6e
Sergey <serggsv8@gmail.com>
2022-11-06 00:36:04 +0300

    Решение Задачи2

---
+++ EX001/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = -9;
+int b = -3;
+int min = 0;
+int max = 0;
+
+if (a < b)
+{
+    min = a;
+    max = b;
+}
+else
+{
+    min = b;
+    max = a;
+}
+Console.Write("min = ");
+Console.WriteLine(min);
+Console.Write("max = ");
+Console.WriteLine(max);
