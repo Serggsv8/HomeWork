c7783bc1cc98b19e67136b45029f10ee83d3bc96
Sergey <serggsv8@gmail.com>
2022-11-06 01:39:59 +0300

    Решение Задачи4

---
+++ EX002/Program.cs
@@ -0,0 +1,15 @@
+﻿int numb1 = 44;
+int numb2 = 5;
+int numb3 = 78;
+int max = numb1;
+
+if(numb2 > max)
+{
+    max = numb2;
+}
+if(numb3 > max)
+{
+    max = numb3;
+}
+Console.Write("max = ");
+Console.WriteLine(max);
