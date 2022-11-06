2e069c5e9eaad061c869b5f0d199c18649ab5f2a
Sergey <serggsv8@gmail.com>
2022-11-06 23:20:22 +0300

    add files temp6

---
+++ EX003/Program.cs
@@ -0,0 +1,13 @@
+﻿int numb = 7;
+string result = "test";
+
+if (numb % 2 == 0)
+{
+    result = "true";
+}
+else
+{
+    result = "false";
+}
+Console.Write("result = ");
+Console.WriteLine(result);
