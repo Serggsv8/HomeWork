4e6ebf81b51b1e1b821d2ac5e5d40bf4346e7e98
Sergey <serggsv8@gmail.com>
2022-11-13 09:09:18 +0300

    Решение задачи 10

---
+++ EX006/Program.cs
@@ -0,0 +1,35 @@
+﻿
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+int CheckNum = -1;
+
+CheckNum = ErrorFind(numb, CheckNum);
+FindThird(CheckNum);
+
+void FindThird(int CheckNum)
+{
+    int a = CheckNum;
+    while (a > 1000)
+    {
+        a = a / 10;
+    }
+    int result = (a % 10);
+    Console.Write("Третья цифра введенного числа: ");
+    Console.WriteLine(result);
+}
+
+int ErrorFind(int numb, int CheckNum)
+{
+    if (numb < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    else
+    {
+        CheckNum = numb;
+    }
+
+    return CheckNum;
+}
