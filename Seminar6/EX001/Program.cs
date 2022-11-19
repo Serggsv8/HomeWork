9bd0bc8f527c8094ceb271b70b822ee2b3a3d688
Sergey <serggsv8@gmail.com>
2022-11-20 02:33:58 +0300

    Решение задач 6 семинара

---
+++ Seminar6/EX001/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите длину массива N: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+Console.WriteLine("Введите i элемент: ");
+
+StepArray(size, array);
+
+ void StepArray(int size, int[] array)
+{
+    double count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+         if (array[i] > 0)
+    {
+        count++;
+    }
+    }
+    Console.Write("Полученный массив: ");
+    Console.WriteLine(string.Join(" ", array));
+
+     Console.Write("Чисел больше 0: ");
+    Console.WriteLine(count);
+}
---
+++ Seminar6/EX002/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double b1, k1, b2, k2;
+
+EnterArgs(out b1, out k1, out b2, out k2);
+
+CheckPoints(b1, k1, b2, k2);
+
+void FindIntersection(double b1, double k1, double b2, double k2)
+{
+    double x = -(b1 - b2) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Координата пересечения: ({x};{y})");
+}
+
+void CheckPoints(double b1, double k1, double b2, double k2)
+{
+    if (k1 == k2)
+    {
+        Console.WriteLine("Эти прямые параллельны");
+    }
+    else
+    {
+        FindIntersection(b1, k1, b2, k2);
+    }
+}
+
+void EnterArgs(out double b1, out double k1, out double b2, out double k2)
+{
+    Console.WriteLine("Введите переменную b1: ");
+    b1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите переменную k1: ");
+    k1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите переменную b2: ");
+    b2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите переменную k2: ");
+    k2 = Convert.ToInt32(Console.ReadLine());
+}
