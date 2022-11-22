1d4b3c41d63e60518f81f99c16f657d59b35f4a2
Sergey <serggsv8@gmail.com>
2022-11-23 01:13:09 +0300

    Решение задач 7 семинара

---
+++ Seminar7/EX001/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+Console.WriteLine("Введите длину массива m: ");
+int m = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Введите длину массива n: ");
+int n = Convert.ToInt16(Console.ReadLine());
+double [,] matrix = new double[m,n];
+
+void PrintArray(double[,] matr)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+ for (int j = 0; j < matrix.GetLength(1); j++)
+ {
+    Console.Write($"{matrix[i, j]} ");
+     }
+Console.WriteLine();
+}
+}
+
+void FillArray(double[,] matr)
+{
+ for (int i = 0; i < matr.GetLength(0); i++)
+ {
+ for (int j = 0; j < matr.GetLength(1); j++)
+ {
+ matr[i,j] = new Random().Next(-100,100) / 10.0;
+ }
+ }
+}
+PrintArray (matrix);
+FillArray(matrix);
+Console.WriteLine();
+Console.WriteLine("Полученная матрица: ");
+PrintArray(matrix);
+
---
+++ Seminar7/EX002/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 17 -> такого числа в массиве нет
+int rows = FindInt("Ввод индекса строки: ");
+int colums = FindInt("Ввод индекса столбца: ");
+
+int [,] matrix = new int[4,4];
+
+Console.Write("Полученный массив: ");
+Console.WriteLine();
+PrintArray (matrix);
+
+if (rows < matrix.GetLength(0) && colums < matrix.GetLength(1)) 
+{
+Console.WriteLine("Искомое число по введенному индексу - " + matrix[rows, colums]);
+}
+else 
+{
+Console.WriteLine($"{rows}{colums} -> такого числа в массиве нет");
+}
+
+void PrintArray(int[,] matr)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+ for (int j = 0; j < matrix.GetLength(1); j++)
+ {
+    matr[i,j] = new Random().Next(0,10);
+    Console.Write($"{matr[i, j]} ");
+     }
+Console.WriteLine();
+}
+}
+
+int FindInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt16(Console.ReadLine());
+}
+
