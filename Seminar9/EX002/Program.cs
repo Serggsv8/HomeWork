4ce4725e29d2914f61e53c1f4abb1d3f1bbdd515
Sergey <serggsv8@gmail.com>
2022-11-26 12:56:27 +0300

    Решение задач семинара 9

---
+++ Seminar9/EX002/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных 
+// элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите начальное число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите конечное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Сумма чисел от " + $"{M} " + "до " + $"{N}: ");
+ResultSum(M, N, 0);
+
+void ResultSum (int M, int N, int sum)
+{
+    if (M > N) 
+    {
+        Console.Write($"{sum}"); 
+        return;
+    }
+    sum = sum + (M++);
+    ResultSum(M, N, sum);
+}
